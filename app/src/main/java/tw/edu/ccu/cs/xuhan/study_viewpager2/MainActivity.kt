92b26e6e959288946866dad889c1b60324a7e69c
Han <xuhan0325@gmail.com>
2024-02-07 23:35:44 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/ccu/cs/xuhan/study_viewpager2/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.ccu.cs.xuhan.study_viewpager2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
