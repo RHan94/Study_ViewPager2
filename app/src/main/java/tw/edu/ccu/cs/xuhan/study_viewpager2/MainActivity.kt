6693cc97bcdca4446099b24d3939f715dcd08ecb
Han <xuhan0325@gmail.com>
2024-02-08 14:09:28 +0800

    Implement indicator using LinearLayout with custom method

--- app/src/main/java/tw/edu/ccu/cs/xuhan/study_viewpager2/MainActivity.kt
+++ app/src/main/java/tw/edu/ccu/cs/xuhan/study_viewpager2/MainActivity.kt
@@ -2,7 +2,11 @@ package tw.edu.ccu.cs.xuhan.study_viewpager2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TableLayout
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -17,11 +21,49 @@ class MainActivity : AppCompatActivity() {
         var imgs = arrayOf(R.drawable.shield,R.drawable.quick,R.drawable.unknown,R.drawable.geolocalization)
         vp.adapter = PagerAdapter(titles,imgs)
 
-        val indicator = findViewById<TabLayout>(R.id.indicator)
+        val indicator = findViewById<LinearLayout>(R.id.indicator)
 
-        TabLayoutMediator(indicator, vp) { tab, position ->
-            tab.text = "${position + 1}"
-        }.attach()
+
+         fun setupIndicators(count: Int) {
+            val indicators = arrayOfNulls<ImageView>(count)
+            val layoutParams: LinearLayout.LayoutParams =
+                LinearLayout.LayoutParams(100, 100)
+            layoutParams.setMargins(8, 0, 8, 0)
+            for (i in indicators.indices) {
+                indicators[i] = ImageView(this).apply {
+                    this.setImageDrawable(ContextCompat.getDrawable(
+                        context,
+                        R.drawable.unselected) // 指定未選中圓點的樣式
+                    )
+                    this.layoutParams = layoutParams
+                }
+                indicator.addView(indicators[i])
+            }
+        }
+
+        setupIndicators(imgs.size)
+
+        fun updateIndicator(position: Int) {
+            val childCount = indicator.childCount
+            for (i in 0 until childCount) {
+                val imageView = indicator.getChildAt(i) as ImageView
+                if (i == position) {
+                    imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.selected)) // 選中狀態
+                } else {
+                    imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.unselected)) // 未選中狀態
+                }
+            }
+        }
+
+        vp.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                updateIndicator(position)
+            }
+        })
 
     }
+
+
+
 }
