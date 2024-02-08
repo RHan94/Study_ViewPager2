d3f856995188418f24560197254b32564b331798
Han <xuhan0325@gmail.com>
2024-02-08 13:57:44 +0800

    Implement indicaotr using TabLayout

--- app/src/main/java/tw/edu/ccu/cs/xuhan/study_viewpager2/MainActivity.kt
+++ app/src/main/java/tw/edu/ccu/cs/xuhan/study_viewpager2/MainActivity.kt
@@ -2,7 +2,10 @@ package tw.edu.ccu.cs.xuhan.study_viewpager2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         var titles = arrayOf("安全","快速","匿名","無國界")
         var imgs = arrayOf(R.drawable.shield,R.drawable.quick,R.drawable.unknown,R.drawable.geolocalization)
         vp.adapter = PagerAdapter(titles,imgs)
+
+        val indicator = findViewById<TabLayout>(R.id.indicator)
+
+        TabLayoutMediator(indicator, vp) { tab, position ->
+            tab.text = "${position + 1}"
+        }.attach()
 
     }
 }
